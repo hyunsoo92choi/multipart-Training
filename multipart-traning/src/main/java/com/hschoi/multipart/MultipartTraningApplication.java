eb8afcff92755d294d770b69494514f318b76db1
hschoi <gu2rago@gmail.com>
2019-10-30 12:50:20 +0900

    first commit

---
+++ multipart-traning/src/main/java/com/hschoi/multipart/MultipartTraningApplication.java
@@ -0,0 +1,13 @@
+package com.hschoi.multipart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipartTraningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipartTraningApplication.class, args);
+	}
+
+}
---
+++ multipart-traning/src/test/java/com/hschoi/multipart/MultipartTraningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hschoi.multipart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipartTraningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
